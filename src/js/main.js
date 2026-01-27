cf5a169c320def134ea35b39ff714da9f8648c77
YousefSamy2003 <yousafsamy9@outlook.com>
2026-01-27 18:09:56 +0200

    HTML+CSS+JS

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+const allLink = document.querySelectorAll(".nav-section .nav-item");
+const allSection = document.querySelectorAll("section");
+
+allLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    allLink.forEach((l) => l.classList.remove("active"));
+    link.classList.add("active");
+    const viewName = link.getAttribute("data-view");
+
+    allSection.forEach((section) => {
+      if (section.getAttribute("data-section") === viewName) {
+        section.classList.remove("view");
+      } else {
+        section.classList.add("view");
+        section.classList.remove("active");
+      }
+    });
+  });
+});
+
+
+
+
+
+
